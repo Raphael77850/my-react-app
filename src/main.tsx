8b3e75adb61e803f0b998e88b4e7fe92cb8f098c
Raphaël Streiff <raphaelstreiff93@gmail.com>
2024-10-23 14:53:40 +0200

    feat: make PokemonCard component dynamic with conditional rendering

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
